6ac12cb983c38a2000ea42dc5c3d1094b3fc1728
obotelho@br.ibm.com <obotelho@br.ibm.com>
2017-11-16 08:46:32 -0200

    New version of model and added state to both origin and destination on query

--- GoogleMaps/GoogleMaps.py
+++ GoogleMaps/GoogleMaps.py
@@ -2,6 +2,15 @@ import googlemaps
 from datetime import datetime
 import csv
 
+ORIGIN_CITY = 0
+ORIGIN_STATE = 1
+DESTINATION_CITY = 2
+DESTINATION_STATE = 3
+DISTANCE = 4
+HOURS = 5
+MINUTES = 6
+DECIMAL_TIME = 7
+
 gmaps = googlemaps.Client(key = open('MapsKey.txt','r',encoding='utf-8').read())
 
 with open('Cities.csv',newline='',encoding='utf-8') as myFile, open('FullCities.csv','w',newline='',encoding='utf-8') as writeFile:
@@ -10,7 +19,7 @@ with open('Cities.csv',newline='',encoding='utf-8') as myFile, open('FullCities.
     #Skip Header
     firstRow = True
     for row in reader:
-        if firstRow == True:
+        if firstRow == True or row[DECIMAL_TIME] != '':
             writer.writerow(row)
             firstRow = False
         else:
@@ -18,31 +27,31 @@ with open('Cities.csv',newline='',encoding='utf-8') as myFile, open('FullCities.
             found = False
             attempts = 0
             while(found == False and attempts <10):
-                directions_result = gmaps.directions(row[0],row[2],mode="driving")
+                directions_result = gmaps.directions('{},{}'.format(row[ORIGIN_CITY],row[ORIGIN_STATE]),'{},{}'.format(row[DESTINATION_CITY],row[DESTINATION_STATE]),mode="driving")
                 if directions_result != []:
                     directions_result = directions_result[0]['legs'][0]
                     print('Road distance: {}'.format(directions_result['distance']['text']))
                     print('Road duration: {}'.format(directions_result['duration']['text']))
 
                     #Store distance
-                    row[3] = directions_result['distance']['text'].split(' ')[0]
+                    row[DISTANCE] = directions_result['distance']['text'].split(' ')[0]
 
                     #Store Time
                     if len(directions_result['duration']['text'].split(' ')) > 2:
                         #Hour
-                        row[4] = directions_result['duration']['text'].split(' ')[0]
+                        row[HOURS] = directions_result['duration']['text'].split(' ')[0]
                         #Minutes
-                        row[5] = directions_result['duration']['text'].split(' ')[2]
+                        row[MINUTES] = directions_result['duration']['text'].split(' ')[2]
                         #Store Hour + decimal(min). e.g. 1h15min = 1.25h
-                        row[6] = str(int(directions_result['duration']['text'].split(' ')[2])/60 + int(row[4]))
+                        row[DECIMAL_TIME] = str(int(directions_result['duration']['text'].split(' ')[2])/60 + int(row[HOURS]))
                     else:
                         #Just minutes
-                        row[5] = directions_result['duration']['text'].split(' ')[0]
-                        row[6] = str(int(row[5])/60)
+                        row[MINUTES] = directions_result['duration']['text'].split(' ')[0]
+                        row[DECIMAL_TIME] = str(int(row[MINUTES])/60)
                     found = True
                 else:
                     attempts = attempts + 1
-                    print('Did not find route from {} and {}. Tried {} times'.format(row[0],row[2],attempts))
+                    print('Did not find route from {} and {}. Tried {} times'.format(row[ORIGIN_CITY],row[DESTINATION_CITY],attempts))
             print('Final row: {}'.format(row))
             writer.writerow(row)
 
